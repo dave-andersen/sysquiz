5d32702c10a891882cae619241e3aae65b99cc0b
David G. Andersen <dave.andersen@gmail.com>
2012-03-24 14:18:34 -0400

    initial import

---
+++ js/script.js
@@ -0,0 +1,147 @@
+/* Author: David G. Andersen
+ *
+ * requires jquery to be loaded already
+ */
+
+var FETCH_QUIZLIST = "Fetching quiz list";
+var CREATE_QUIZ = "Creating quiz";
+var FETCH_QUIZ = "Fetching quiz data";
+var SAVE_QUIZ = "Saving quiz";
+var userid = "userid";
+
+$(document).ready(function()
+{
+//    $("#qbox").keyup(doQuery);
+    fetchQuizList();
+    $("#create_btn").click(createQuiz);
+    $("#save_btn").click(saveQuiz);
+    $("#newitem").click(newItem);
+    make_items_sortable();
+});
+
+function make_items_sortable() {
+    $("#questionlist").sortable({
+	start: function(event, ui) {
+	    draggable_sibling = $(ui.item).prev();
+	},
+	stop: function(event, ui) {
+	}
+    });
+}
+
+function newItem() {
+    $("#questionlist").append($("#question_input").html())
+    return false;
+}
+
+function parse_question(el) {
+    var q = new Object();
+    q.Text = el.find('#question_text').val();
+    return q;
+}
+
+function saveQuiz() {
+    var q = new Object();
+    q.ID = $('#edit_id').val();
+    q.Title = $('#edit_name').val();
+    q.Questions = new Array();
+    $('#questionlist li').each(function(i, el) {
+	q.Questions[i] = parse_question($(this));
+    });
+    status(SAVE_QUIZ);
+    as_string = JSON.stringify(q);
+    $.post("/qu", {q: as_string}, saveQuizDone, "json");
+    return false;
+}
+
+function saveQuizDone(r) {
+    if (r["status"] == "failed") {
+	alert("saving changes failed!");
+    }
+    removeStatus(SAVE_QUIZ);
+    fetchQuizList();
+}
+
+function createQuiz() {
+    $("label#name_error").hide();
+    
+    var qname = $(document).find('input[id="name"]').val()
+    var label_error = ""
+    if (qname == "") {
+	label_error = "Name can't be blank";
+    }
+    if (label_error != "") {
+	$("label#name_error").text(label_error);
+	$("label#name_error").show();
+	$("input#name").focus();
+	return false;
+    }
+    status(CREATE_QUIZ);
+    $.post("/qc", {qname: qname }, quizCreateDone, "json");
+    return false;
+}
+
+function quizCreateDone(r) {
+    $("input#name").val('')
+    removeStatus(CREATE_QUIZ)
+    fetchQuizList();
+}
+
+function fetchQuizList() {
+    status(FETCH_QUIZLIST)
+    $.post("/ql", {u: userid }, gotQuizList, "json");
+}
+function gotQuizList(r) {
+    removeStatus(FETCH_QUIZLIST)
+    if (r['status'] == "AUTH") {
+	$('#mainstatus').html("Need to login!  Go to <a href='/admin'>/admin</a> for now");
+	return false
+    }
+    var ql = r['quizlist']
+    $('#quizlist').empty()
+    for (var i = 0; i < ql.length; i++) {
+	var editlink = "editlink_"+ql[i].ID;
+	$('#quizlist').append('<li><a href="#edit" id="'+editlink+'">'+ql[i].Title+'</a> - '+ql[i].ID+' ('+ql[i].Created+')</li>');
+	$('#'+editlink).click({id: ql[i].ID}, editQuiz);
+    }
+}
+
+function editQuiz(event) {
+    quizID = event.data.id;
+    status(FETCH_QUIZ);
+    $.post("/qget", {q: quizID}, editQuizGotQuizInfo, "json");
+    return false;
+}
+
+function editQuizGotQuizInfo(r) {
+    quiz = r['quiz'];
+    $('#edit_name').val(quiz.Title);
+    $('#edit_id').val(quiz.ID);
+    $('#edit').show();
+    $('#questionlist').empty()
+    if (quiz.Questions) {
+	qhtml = $('#question_input').html()
+	for (var i = 0; i < quiz.Questions.length; i++) {
+	    var q = quiz.Questions[i];
+	    $('#questionlist').append(qhtml);
+	    el = $('#questionlist li').last() // xxx - this is n^2. :(
+	    el.find("#question_text").val(q.Text)
+	}
+    }
+}
+
+function doQuery() {
+    var qdat = $(document).find('input[id="qbox"]').val();
+    $.post("/q", {q: qdat }, gotResult, "json");
+}
+
+function gotResult(r) {
+    $('#val').text(r['val'])
+}
+
+function status(s) {
+    $('#status').text(s)
+}
+function removeStatus(s) {
+    $('#status').text('')
+}
---
+++ myapp/myapp.go
@@ -0,0 +1,216 @@
+package myapp
+
+import (
+	"net/http"
+	"text/template"
+	"strconv"
+	"encoding/json"
+	"crypto/rand"
+	"encoding/base32"
+	"html"
+	"fmt"
+	"appengine"
+	"appengine/datastore"
+	"appengine/user"
+	"time"
+)
+
+type Page struct {
+     Title string
+     Content string
+}
+
+type Question struct {
+	Text string
+	Answer_Options []string // for multiple choice type questions
+	Answer string
+	Answer_Type string // "text", "int", "float", ...
+	Is_Stop bool // Must they answer this question before going on?
+}
+
+type Quiz struct {
+	Title string
+	ID string
+	Created time.Time
+	OwnerID string  `json:"-"`  // ownerID not sent via JSON
+	Questions_m string // marshaled into JSON for storage in the datastore
+}
+
+type Quize struct { // ugly.  just a Quiz, but with Questions in correct format.
+	Title string
+	ID string
+	Created time.Time
+	OwnerID string  `json:"-"`  // ownerID not sent via JSON
+	Questions []Question
+}
+
+var (
+	pageTemplate = template.Must(template.ParseFiles("page.html", "boring.html"))
+	adminTemplate = template.Must(template.ParseFiles("page.html", "admin.html"))
+	instanceHitCount = 0
+)
+
+
+func genQuizID() string {
+	b := make([]byte, 16)
+	n, err := rand.Read(b)
+	if n != 16 {
+		panic(fmt.Sprintf("could not read 16 random bytes.  I'm very unhappy: %s", err))
+	}
+	return base32.StdEncoding.EncodeToString(b)[0:24]
+}
+
+func init() {
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/admin", adminHandler)
+	http.HandleFunc("/q", queryHandler)
+	http.Handle("/ql", AuthHandlerFunc(quizListHandler))
+	http.Handle("/qc", AuthHandlerFunc(quizCreateHandler))
+	http.Handle("/qget", AuthHandlerFunc(quizGetHandler))
+	http.Handle("/qu", AuthHandlerFunc(quizUpdateHandler))
+}
+
+type AuthHandlerFunc func(http.ResponseWriter, *http.Request, appengine.Context, *user.User, map[string]interface{})
+
+func (f AuthHandlerFunc) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	jsonAuthHandler(w, r, f)
+}
+
+func jsonAuthHandler(w http.ResponseWriter, r *http.Request, handler func(http.ResponseWriter, *http.Request, appengine.Context, *user.User, map[string]interface{})) {
+	w.Header().Set("Content-Type", "text/javascript")
+	c := appengine.NewContext(r)
+	u := user.Current(c)
+	resp := make(map[string]interface{})
+	if u == nil {
+		c.Infof("jsonAuthHandler punting:  no valid user for %v", r.URL)
+		resp["status"] = "AUTH"
+	} else {
+		handler(w, r, c, u, resp)
+	}
+	b, _ := json.Marshal(resp)
+	w.Write(b)
+	return
+}
+
+func quizUpdateHandler(w http.ResponseWriter, r *http.Request, c appengine.Context, u *user.User, resp map[string]interface{}) {
+	var q Quiz;
+	var nq Quize;
+	if err := json.Unmarshal([]byte(r.FormValue("q")), &nq); err != nil {
+		c.Infof("Unmarshal json failed on %v", r.FormValue("q"))
+		resp["status"] = "json failed";
+		return;
+	}
+	quizID := nq.ID;
+	k := datastore.NewKey(c, "Quiz", quizID, 0, nil)
+	// sanity check quizID, please
+	if err := datastore.Get(c, k, &q); err != nil {
+		resp["status"] = "Failed";
+		return
+	}
+	if q.OwnerID != u.ID {
+		resp["status"] = "AUTH";
+		return
+	}
+	// Sanitize the incoming quiz.  ALL FIELDS - including deep into questions
+	// BE CAREFUL HERE.  Likely place to introduce xss vuln.
+	q.Title = html.EscapeString(nq.Title);
+	for i, qu := range nq.Questions {
+		nq.Questions[i].Text = html.EscapeString(qu.Text)
+	}
+	qm, _ := json.Marshal(nq.Questions)
+	q.Questions_m = string(qm)
+
+	if _, err := datastore.Put(c, k, &q); err == nil {
+		resp["status"] = "ok"
+	} else {
+		c.Infof("Could not put new quiz: %v", err)
+		resp["status"] = "failed"
+	}
+}
+
+func quizCreateHandler(w http.ResponseWriter, r *http.Request, c appengine.Context, u *user.User, resp map[string]interface{}) {
+	qname := r.FormValue("qname")
+	qname = html.EscapeString(qname)
+	// validate
+	q := &Quiz{qname, genQuizID(), time.Now(), u.ID, ""}
+	k := datastore.NewKey(c, "Quiz", q.ID, 0, nil)
+	if _, err := datastore.Put(c, k, q); err == nil {
+		resp["status"] = "ok"
+	} else {
+		resp["status"] = "failed"
+	}
+}
+
+func queryHandler(w http.ResponseWriter, r *http.Request) {
+	resp := make(map[string]string)
+	resp["val"] = r.FormValue("q")
+	w.Header().Set("Content-Type", "text/javascript")
+	b, err := json.Marshal(resp)
+	if err == nil {
+		w.Write(b)
+	}
+}
+
+func quizGetHandler(w http.ResponseWriter, r *http.Request, c appengine.Context, u *user.User, resp map[string]interface{}) {
+	var q Quiz
+	quizID := r.FormValue("q")
+	k := datastore.NewKey(c, "Quiz", quizID, 0, nil)
+	// sanity check quizID, please
+	if err := datastore.Get(c, k, &q); err != nil {
+		resp["status"] = "Failed";
+		return
+	}
+	if q.OwnerID != u.ID {
+		resp["status"] = "AUTH";
+		return
+	}
+	qe := &Quize{q.Title, q.ID, q.Created, "", []Question{}}
+	json.Unmarshal([]byte(q.Questions_m), &qe.Questions)
+	resp["status"] = "ok"
+	resp["quiz"] = qe
+}
+
+func quizListHandler(w http.ResponseWriter, r *http.Request, c appengine.Context, u *user.User, resp map[string]interface{}) {
+	var qlist []Quiz = make([]Quiz, 0)
+	var status = "ok"
+	q := datastore.NewQuery("Quiz").Filter("OwnerID =", u.ID).Order("Created")
+	for t := q.Run(c); ; {
+		var quiz Quiz
+		_, err := t.Next(&quiz)
+		if err == datastore.Done {
+			break
+		}
+		if err != nil {
+			c.Infof("datastore query failed: %v", err)
+			status = "failed"
+			break
+		}
+		qlist = append(qlist, quiz)
+	}
+
+	resp["quizlist"] = qlist
+	resp["status"] = status
+}
+
+func adminHandler(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+	u := user.Current(c)
+	var p *Page
+	if u == nil {
+		url, _ := user.LoginURL(c, "/admin")
+		p = &Page{Title: "Authentication required",
+		Content: fmt.Sprintf(`<a href="%s">Sign in or register</a>`, url)}
+	} else {
+		logouturl, _ := user.LogoutURL(c, "/admin")
+		p = &Page{Title: "Admin",
+		Content: fmt.Sprintf("Hi, %s, this is your admin page. <a href='%s'>logout</a> or <a href='/'>back to main</a>",u, logouturl) }
+	}
+	adminTemplate.Execute(w, p)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	instanceHitCount++
+	p := &Page{Title: "Quizzer",
+	Content: "Nothing to see here ("+strconv.Itoa(instanceHitCount)+" instance visits)"}
+	pageTemplate.Execute(w, p)
+}
