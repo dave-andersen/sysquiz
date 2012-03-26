dbb9b78b4c3c0c227765e57b1a789ce7d3e645c9
Dave Andersen <dga@cs.cmu.edu>
2012-03-26 13:03:02 -0500

    namespaces are nice, I hear.

--- js/script.js
+++ js/script.js
@@ -9,49 +9,52 @@ var FETCH_QUIZ = "Fetching quiz data";
 var SAVE_QUIZ = "Saving quiz";
 var userid = "userid";
 
+/* Namespace for this app */
+var sysquiz = {};
+
 $(document).ready(function()
 {
-    fetchQuizList();
-    $("#create_btn").click(createQuiz);
-    $("#save_btn").click(saveQuiz);
-    $("#newitem").click(newItem);
+    sysquiz.fetchQuizList();
+    $("#create_btn").click(sysquiz.createQuiz);
+    $("#save_btn").click(sysquiz.saveQuiz);
+    $("#newitem").click(sysquiz.newItem);
     $("#questionlist").sortable();
 });
 
-function newItem(event) {
+sysquiz.newItem = function(event) {
     event.preventDefault();
-    appendNewQuestion($('#questionlist'));
+    sysquiz.appendNewQuestion($('#questionlist'));
 }
 
-function parseQuestion(el) {
+sysquiz.parseQuestion = function(el) {
     var q = { Text: el.find('#question_text').val(),
 	      AnswerType: el.find('#AnswerType').val() };
     return q;
 }
 
-function saveQuiz(event) {
+sysquiz.saveQuiz = function(event) {
     event.preventDefault();
     var q = { ID: $('#edit_id').val(),
 	      Title: $('#edit_name').val(),
 	      Questions: new Array() };
 
     $('#questionlist li').each(function(i, el) {
-	q.Questions[i] = parseQuestion($(this));
+	q.Questions[i] = sysquiz.parseQuestion($(this));
     });
     var as_string = JSON.stringify(q);
-    status(SAVE_QUIZ);
-    $.post("/qu", {q: as_string}, saveQuizDone, "json");
+    sysquiz.status(SAVE_QUIZ);
+    $.post("/qu", {q: as_string}, sysquiz.saveQuizDone, "json");
 }
 
-function saveQuizDone(r) {
+sysquiz.saveQuizDone = function(r) {
     if (r["error"]) {
 	alert("saving changes failed: " + r["error"].Message);
     }
-    removeStatus(SAVE_QUIZ);
-    fetchQuizList();
+    sysquiz.removeStatus(SAVE_QUIZ);
+    sysquiz.fetchQuizList();
 }
 
-function createQuiz(event) {
+sysquiz.createQuiz = function(event) {
     event.preventDefault();
     var neLabel = $("#createquiz label#name_error");
     var nameInput = $('input#name');
@@ -67,23 +70,23 @@ function createQuiz(event) {
 	nameInput.focus();
 	return;
     }
-    status(CREATE_QUIZ);
-    $.post("/qc", {qname: qname }, createQuizDone, "json");
+    sysquiz.status(CREATE_QUIZ);
+    $.post("/qc", {qname: qname }, sysquiz.createQuizDone, "json");
 }
 
-function createQuizDone(r) {
+sysquiz.createQuizDone = function(r) {
     $("input#name").val('')
-    removeStatus(CREATE_QUIZ)
-    fetchQuizList();
+    sysquiz.removeStatus(CREATE_QUIZ)
+    sysquiz.fetchQuizList();
 }
 
-function fetchQuizList() {
-    status(FETCH_QUIZLIST)
-    $.post("/ql", {u: userid }, fetchQuizListDone, "json");
+sysquiz.fetchQuizList = function() {
+    sysquiz.status(FETCH_QUIZLIST)
+    $.post("/ql", {u: userid }, sysquiz.fetchQuizListDone, "json");
 }
 
-function fetchQuizListDone(r) {
-    removeStatus(FETCH_QUIZLIST)
+sysquiz.fetchQuizListDone = function(r) {
+    sysquiz.removeStatus(FETCH_QUIZLIST)
     if (r['error']) {
 	var e = r['error'];
 	if (e.code == 401) {
@@ -99,18 +102,18 @@ function fetchQuizListDone(r) {
     for (var i = 0; i < ql.length; i++) {
 	var editlink = "editlink_"+ql[i].ID;
 	pageQl.append('<li><a href="#edit" id="'+editlink+'">'+ql[i].Title+'</a> - '+ql[i].ID+' ('+ql[i].Created+')</li>');
-	$('#'+editlink).click({id: ql[i].ID}, editQuiz);
+	$('#'+editlink).click({id: ql[i].ID}, sysquiz.editQuiz);
     }
 }
 
-function editQuiz(event) {
+sysquiz.editQuiz = function(event) {
     event.preventDefault();
     var quizID = event.data.id;
-    status(FETCH_QUIZ);
-    $.post("/qget", {q: quizID}, editQuizGotQuizInfo, "json");
+    sysquiz.status(FETCH_QUIZ);
+    $.post("/qget", {q: quizID}, sysquiz.editQuizGotQuizInfo, "json");
 }
 
-function appendNewQuestion(ql) {
+sysquiz.appendNewQuestion = function(ql) {
     var qhtml = $('#question_input').html()
     ql.append(qhtml);
     var el = ql.find('li').last();  // xxx - this is n^2. :(
@@ -121,9 +124,9 @@ function appendNewQuestion(ql) {
     return el;
 }
 
-function editQuizGotQuizInfo(r) {
+sysquiz.editQuizGotQuizInfo = function(r) {
     var quiz = r['quiz'];
-    $("#revert_btn").click({id: quiz.ID}, editQuiz);
+    $("#revert_btn").unbind('click').click({id: quiz.ID}, sysquiz.editQuiz);
     $('#edit_name').val(quiz.Title);
     $('#edit_id').val(quiz.ID);
     $('#edit').show();
@@ -132,7 +135,7 @@ function editQuizGotQuizInfo(r) {
     if (quiz.Questions) {
 	for (var i = 0; i < quiz.Questions.length; i++) {
 	    var q = quiz.Questions[i];
-	    var el = appendNewQuestion(ql);
+	    var el = sysquiz.appendNewQuestion(ql);
 	    el.find("#question_text").val(q.Text)
 	    // throw in an answer div so we can delete the whole thing
 	    // if they change the type...
@@ -141,16 +144,16 @@ function editQuizGotQuizInfo(r) {
     }
 }
 
-function status(s) {
+sysquiz.status = function(s) {
     $('#status').text(s)
 }
-function removeStatus(s) {
+sysquiz.removeStatus = function(s) {
     $('#status').text('')
 }
 
 // Create HTML snippets for different answer types and bind them
 // to appropriate validators
-function appendAnswerDuration(el) {
+sysquiz.appendAnswerDuration = function(el) {
     el.append($('#widgets #duration'));
     el.append($('#widgets #DurationUnits'));
 }
